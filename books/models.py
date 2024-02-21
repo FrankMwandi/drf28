79f885c793b904a9406be38ac266e85cc848c1c7
mwandi <frankmwandi93@gmail.com>
2024-02-21 15:30:22 +0300

    added uuid

--- books/models.py
+++ books/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+import uuid
 from django.urls import reverse 
 class Book(models.Model):
+    id = models.UUIDField(
+        primary_key = True,
+        default = uuid.uuid4,
+        editable = False
+    )
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=6, decimal_places=2)
