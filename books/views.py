89882f9de651e9ee9c2815dde4dd234deaa9503f
mwandi <frankmwandi93@gmail.com>
2024-03-02 21:58:07 +0300

    basic search with q

--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Book
+from django.db.models import Q
 from django.contrib.auth.mixins import (
     LoginRequiredMixin, 
     PermissionRequiredMixin
@@ -18,3 +19,14 @@ class BookDetailView(LoginRequiredMixin,DetailView):
     template_name = "books/book_detail.html"
     login_url = "account_login"
     permission_required = "book.special_status"
+    
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = "book_list"
+    template_name = "books/search_results.html"
+    
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        return Book.objects.filter(
+            Q(title__icontains=query)|Q(title__icontains=query)
+        )
